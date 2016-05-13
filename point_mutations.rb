a177d965425377dfc06565deeebc89a99494da34
jleewebdev <lee45276@gmail.com>
2016-05-13 11:36:20 +0800

    All tests pass

--- point_mutations.rb
+++ point_mutations.rb
@@ -1,7 +1,17 @@
 class DNA 
+  attr_accessor :sequence
 
-  def hamming_distance
-    0
+  def initialize(sequence)
+    @sequence = sequence
   end
 
+  def hamming_distance(compare_sequence)
+    differences = 0
+    @sequence.chars.each_with_index do |char, index|
+      compare_letter = compare_sequence[index]
+      break unless compare_letter
+      differences += 1 unless char == compare_letter
+    end
+    differences
+  end
 end
